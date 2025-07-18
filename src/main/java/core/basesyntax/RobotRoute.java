26b3a8bea356ca5264587b77c812e18e5d080469
kraievskyi-om <kraievskyi-om@ukrposhta.com>
2025-07-18 13:49:10 +0300

    Implement robot movement to reach target coordinates

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,20 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        Direction directionX = robot.getX() < toX ? Direction.RIGHT : Direction.LEFT;
+        Direction directionY = robot.getY() < toY ? Direction.UP : Direction.DOWN;
+
+        while (directionX != robot.getDirection()) {
+            robot.turnRight();
+        }
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+        while (directionY != robot.getDirection()) {
+            robot.turnRight();
+        }
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
     }
 }
